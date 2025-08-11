dcad373aa565a88eee59f1ddc1f32a8a9b4bb797
SkyDiver23rus <lvv10@yandex.ru>
2025-08-11 13:41:25 +0300

    new UserControllerTest 1.1

--- src/test/java/ru/yandex/practicum/filmorate/controller/UserControllerValidationTest.java
+++ src/test/java/ru/yandex/practicum/filmorate/controller/UserControllerValidationTest.java
@@ -1,85 +1,32 @@
 package ru.yandex.practicum.filmorate.controller;
 
 import org.junit.jupiter.api.Test;
-import ru.yandex.practicum.filmorate.exception.ValidationException;
-import ru.yandex.practicum.filmorate.model.User;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
-import java.time.LocalDate;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
-import static org.junit.jupiter.api.Assertions.*;
+class UserControllerTest {
 
-class UserControllerValidationTest {
-
-    private final UserController controller = new UserController();
-
-    @Test
-    void validateUserThrowIfEmailIsEmpty() {
-        User user = new User();
-        user.setEmail("");
-        user.setLogin("login");
-        user.setName("name");
-        user.setBirthday(LocalDate.of(2000, 1, 1));
-
-        ValidationException exception = assertThrows(ValidationException.class, () -> controller.addUser(user));
-        assertTrue(exception.getMessage().contains("Электронная почта"));
-    }
+    private final MockMvc mockMvc = MockMvcBuilders.standaloneSetup(new UserController()).build();
 
     @Test
-    void validateUserThrowIfEmailWithout() {
-        User user = new User();
-        user.setEmail("mailmail.com");
-        user.setLogin("login");
-        user.setName("name");
-        user.setBirthday(LocalDate.of(2000, 1, 1));
+    void shouldSetLoginAsNameIfNameIsBlank() throws Exception {
+        String json = """
+            {
+                "email": "user@example.com",
+                "login": "testuser",
+                "name": "",
+                "birthday": "2000-01-01"
+            }
+        """;
 
-        ValidationException exception = assertThrows(ValidationException.class, () -> controller.addUser(user));
-        assertTrue(exception.getMessage().contains("Электронная почта"));
-    }
-
-    @Test
-    void validateUserLoginEmpty() {
-        User user = new User();
-        user.setEmail("mail@mail.com");
-        user.setLogin("");
-        user.setName("name");
-        user.setBirthday(LocalDate.of(2000, 1, 1));
-
-        ValidationException exception = assertThrows(ValidationException.class, () -> controller.addUser(user));
-        assertTrue(exception.getMessage().contains("Логин"));
-    }
-
-    @Test
-    void validateUserLoginWithSpace() {
-        User user = new User();
-        user.setEmail("mail@mail.com");
-        user.setLogin("lo gin");
-        user.setName("name");
-        user.setBirthday(LocalDate.of(2000, 1, 1));
-
-        ValidationException exception = assertThrows(ValidationException.class, () -> controller.addUser(user));
-        assertTrue(exception.getMessage().contains("Логин"));
-    }
-
-    @Test
-    void validateUserBirthdayInFuture() {
-        User user = new User();
-        user.setEmail("mail@mail.com");
-        user.setLogin("login");
-        user.setName("name");
-        user.setBirthday(LocalDate.now().plusDays(1));
-
-        ValidationException exception = assertThrows(ValidationException.class, () -> controller.addUser(user));
-        assertTrue(exception.getMessage().contains("Дата рождения"));
-    }
-
-    @Test
-    void validateUserPassIfAllOk() {
-        User user = new User();
-        user.setEmail("mail@mail.com");
-        user.setLogin("login");
-        user.setName("name");
-        user.setBirthday(LocalDate.of(2000, 1, 1));
-
-        assertDoesNotThrow(() -> controller.addUser(user));
+        mockMvc.perform(post("/users")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(json))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.name").value("testuser"));
     }
 }
