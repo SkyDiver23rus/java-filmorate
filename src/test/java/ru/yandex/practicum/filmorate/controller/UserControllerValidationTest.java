f14ff1b51a74e516c6e6ea553b04002c28d8e0af
SkyDiver23rus <lvv10@yandex.ru>
2025-08-11 13:45:45 +0300

    fix tests 1.1

--- src/test/java/ru/yandex/practicum/filmorate/controller/UserControllerValidationTest.java
+++ src/test/java/ru/yandex/practicum/filmorate/controller/UserControllerValidationTest.java
@@ -8,20 +8,20 @@ import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
-class UserControllerTest {
+class UserControllerValidationTest {
 
     private final MockMvc mockMvc = MockMvcBuilders.standaloneSetup(new UserController()).build();
 
     @Test
     void shouldSetLoginAsNameIfNameIsBlank() throws Exception {
         String json = """
-            {
-                "email": "user@example.com",
-                "login": "testuser",
-                "name": "",
-                "birthday": "2000-01-01"
-            }
-        """;
+                    {
+                        "email": "user@example.com",
+                        "login": "testuser",
+                        "name": "",
+                        "birthday": "2000-01-01"
+                    }
+                """;
 
         mockMvc.perform(post("/users")
                         .contentType(MediaType.APPLICATION_JSON)
