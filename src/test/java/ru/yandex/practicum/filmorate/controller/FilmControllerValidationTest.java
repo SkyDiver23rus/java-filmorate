b5eeecdf352c875abc7fb8e850302780083be4c5
SkyDiver23rus <lvv10@yandex.ru>
2025-08-28 12:51:00 +0300

    fix tests 7.4

--- src/test/java/ru/yandex/practicum/filmorate/controller/FilmControllerValidationTest.java
+++ src/test/java/ru/yandex/practicum/filmorate/controller/FilmControllerValidationTest.java
@@ -5,6 +5,7 @@ import ru.yandex.practicum.filmorate.exception.ValidationException;
 import ru.yandex.practicum.filmorate.model.Film;
 import ru.yandex.practicum.filmorate.service.FilmService;
 import ru.yandex.practicum.filmorate.storage.film.InMemoryFilmStorage;
+import ru.yandex.practicum.filmorate.storage.user.InMemoryUserStorage;
 
 import java.time.LocalDate;
 
@@ -14,7 +15,8 @@ class FilmControllerValidationTest {
 
     private final FilmController controller = new FilmController(
             new FilmService(new InMemoryFilmStorage()),
-            new InMemoryFilmStorage()
+            new InMemoryFilmStorage(),
+            new InMemoryUserStorage()
 
     );
 
