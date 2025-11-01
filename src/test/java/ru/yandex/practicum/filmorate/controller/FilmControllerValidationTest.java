2e0d29eef0f9c284e765f068a296cfeed4ed922c
SkyDiver23rus <lvv10@yandex.ru>
2025-11-01 16:38:54 +0300

    fix controller tests

--- src/test/java/ru/yandex/practicum/filmorate/controller/FilmControllerValidationTest.java
+++ src/test/java/ru/yandex/practicum/filmorate/controller/FilmControllerValidationTest.java
@@ -2,23 +2,48 @@ package ru.yandex.practicum.filmorate.controller;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 import ru.yandex.practicum.filmorate.exception.ValidationException;
 import ru.yandex.practicum.filmorate.model.Film;
+import ru.yandex.practicum.filmorate.model.Mpa;
 import ru.yandex.practicum.filmorate.service.FilmService;
+import ru.yandex.practicum.filmorate.storage.DAO.GenreDbStorage;
+import ru.yandex.practicum.filmorate.storage.DAO.MpaDbStorage;
 import ru.yandex.practicum.filmorate.storage.film.InMemoryFilmStorage;
 import ru.yandex.practicum.filmorate.storage.user.InMemoryUserStorage;
 
 import java.time.LocalDate;
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class FilmControllerValidationTest {
 
     private FilmController controller;
+    private MpaDbStorage mpaDbStorage;
+    private GenreDbStorage genreDbStorage;
 
     @BeforeEach
     void setUp() {
-        FilmService filmService = new FilmService(new InMemoryFilmStorage(), new InMemoryUserStorage());
+        mpaDbStorage = Mockito.mock(MpaDbStorage.class);
+        genreDbStorage = Mockito.mock(GenreDbStorage.class);
+
+        // по умолчанию любой MPA "найден"
+        Mockito.when(mpaDbStorage.getMpaById(Mockito.anyInt()))
+                .thenAnswer(invocation -> {
+                    int id = invocation.getArgument(0);
+                    Mpa mpa = new Mpa();
+                    mpa.setId(id);
+                    mpa.setName("Test");
+                    return Optional.of(mpa);
+                });
+
+        FilmService filmService = new FilmService(
+                new InMemoryFilmStorage(),
+                new InMemoryUserStorage(),
+                genreDbStorage,
+                mpaDbStorage
+        );
         controller = new FilmController(filmService);
     }
 
@@ -80,6 +105,7 @@ class FilmControllerValidationTest {
         film.setReleaseDate(LocalDate.of(2000, 1, 1));
         film.setDuration(10);
 
+        // MPA по умолчанию будет найден через мок
         assertDoesNotThrow(() -> controller.addFilm(film));
     }
 }
