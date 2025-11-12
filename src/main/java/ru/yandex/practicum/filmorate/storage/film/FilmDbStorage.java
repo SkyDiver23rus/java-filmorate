928e4662e8ef7c58cb149ddbab09872f467aebc5
SkyDiver23rus <lvv10@yandex.ru>
2025-11-12 22:13:05 +0300

    fix test1

--- src/main/java/ru/yandex/practicum/filmorate/storage/film/FilmDbStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/film/FilmDbStorage.java
@@ -6,7 +6,6 @@ import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.support.GeneratedKeyHolder;
 import org.springframework.jdbc.support.KeyHolder;
 import org.springframework.stereotype.Repository;
-import ru.yandex.practicum.filmorate.model.Director;
 import ru.yandex.practicum.filmorate.model.Film;
 import ru.yandex.practicum.filmorate.model.Genre;
 import ru.yandex.practicum.filmorate.model.Mpa;
@@ -363,4 +362,5 @@ public class FilmDbStorage implements FilmStorage {
             throw new RuntimeException("Database error while deleting film", e);
         }
     }
+
 }
