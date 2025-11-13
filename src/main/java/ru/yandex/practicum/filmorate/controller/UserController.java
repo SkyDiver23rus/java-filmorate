d7d62784ffc61eb2eeb20c87f87ce4c92f4d738e
SkyDiver23rus <lvv10@yandex.ru>
2025-11-13 11:35:07 +0300

    Update UserController.java fix style 3

--- src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
@@ -1,7 +1,6 @@
 package ru.yandex.practicum.filmorate.controller;
 
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import ru.yandex.practicum.filmorate.model.User;
@@ -69,4 +68,5 @@ public class UserController {
         List<Film> recommended = filmService.getRecommendedFilms(id);
         return ResponseEntity.ok(recommended);
     }
+
 }
